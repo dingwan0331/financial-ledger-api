264a8ae646d6f49e4847f7015810eced38b6f638
dingwan <dingwan0331@gmail.com>
2022-12-27 18:07:40 +0900

    feat: Transaction 모델 작성
    - TimeStampModel을 상속 받아서 자겅
    - index처리를 위해 transaction_date 는 IntegerField로 작성

--- apps/transaction/models.py
+++ apps/transaction/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from ..util.models import TimeStampModel
+
+class Transaction(TimeStampModel):
+    deposit          = models.IntegerField()
+    # Index처리를위해 IntegerField 사용
+    transaction_date = models.IntegerField()
+    title            = models.CharField(max_length = 20)
+    description      = models.CharField(max_length = 100)
+    user             = models.ForeignKey('auth.User', on_delete = models.CASCADE)
+
+    class Meta():
+        db_table = 'transactions'
