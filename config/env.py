def04ff55344f58f39c3838ed17db8c5c92593cd
dingwan <dingwan0331@gmail.com>
2022-12-27 17:18:02 +0900

    feat: 프로젝트 초기셋팅
    - 환경변수를 관리하는 env.py 작성
    - 환경변수 .env로 분리
    - manage.py에 dotenv_load 함수 작성
    - corsheaders 설정
    - database logging 설정
    - APPEND_SLASH=False
    - TIME_ZONE= Asia/Seoul로 설정

---
+++ config/env.py
@@ -0,0 +1,9 @@
+import os 
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+MYSQL_PASSWORD = os.environ['MYSQL_PASSWORD']
+MYSQL_DATABASE = os.environ['MYSQL_DATABASE']
+MYSQL_USER     = os.environ['MYSQL_USER']
+MYSQL_HOST     = os.environ['MYSQL_HOST']
+MYSQL_PORT     = os.environ['MYSQL_PORT']
