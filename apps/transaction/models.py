70edd627429c94e75d32bab0727f50da6d16a97c
dingwan <dingwan0331@gmail.com>
2022-12-30 01:12:40 +0900

    feat: transaction manager 추가
    -해당 유저의  제일 마지막 transaction의 잔액을 기준으로 새 거래기록의 잔액을 처리
    - 하나의 트랜잭션에서 처리

--- apps/transaction/models.py
+++ apps/transaction/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-from apps.util.models import TimeStampModel
+from apps.util.models          import TimeStampModel
+from apps.transaction.managers import TransactionManager
 
 class Transaction(TimeStampModel):
     deposit          = models.BigIntegerField()
     title            = models.CharField(max_length = 20)
     description      = models.CharField(max_length = 100)
     user             = models.ForeignKey('auth.User', on_delete = models.CASCADE)
+
+    objects = TransactionManager()
 
     class Meta():
         db_table = 'transactions'
