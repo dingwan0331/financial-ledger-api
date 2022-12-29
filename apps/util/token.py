9381bfba41f2ad7684c67a987cd4472971e79d53
dingwan <dingwan0331@gmail.com>
2022-12-29 18:54:50 +0900

    feat: 토큰 유효성 검사 데코리에터인 verify_token 작성
    - 잘못된 토큰 or 로그아웃 시 Invlid token 반환
    - 토큰 기간 만료시 Expired token 반환

--- apps/util/token.py
+++ apps/util/token.py
@@ -23,3 +23,53 @@ class Token:
         token = jwt.encode(payload, SECRET_KEY)
 
         return token
+
+import jwt
+
+from datetime import datetime, timedelta
+
+from django.core.cache import cache
+
+from config.env          import SECRET_KEY, REFRESH_TOKEN_SECRET_KEY
+from apps.util.exeptions import UnauthorizedException
+
+class Token:
+    def __init__(self):
+        self.EXPIRES = {
+            'access_token' : timedelta(hours = 1), 
+            'refresh_token' : timedelta(hours = 8)
+            }
+        self.SECRET_KEYS = {
+            'access_token' : SECRET_KEY , 
+            'refresh_token' : REFRESH_TOKEN_SECRET_KEY
+            }
+        
+    def sign_token(self, user_id, type):
+        expire     = datetime.utcnow() + self.EXPIRES[type]
+        payload    = {"id" : user_id, "exp" : expire }
+        SECRET_KEY = self.SECRET_KEYS[type]
+
+        token = jwt.encode(payload, SECRET_KEY)
+
+        return token
+
+def verify_token(func):
+    def wrapper(self,request,*args,**kwargs):
+        try:
+            access_token = request.headers.get("Authorization")
+            payload = jwt.decode(access_token, SECRET_KEY, 'HS256')
+
+            refresh_token = cache.get(access_token)
+
+            if refresh_token == 'logout':
+                raise UnauthorizedException('Invalid token')
+            
+            return func(self, request, *args, **kwargs)
+
+        except jwt.exceptions.DecodeError:
+            raise UnauthorizedException('Invalid token')
+
+        except jwt.exceptions.ExpiredSignatureError:
+            return UnauthorizedException('Expired token')
+            
+    return wrapper
