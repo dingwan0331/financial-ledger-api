babe5bbd7409d6e2d56f3f3f4408672bf2efdc80
dingwan <dingwan0331@gmail.com>
2022-12-29 16:51:01 +0900

    feat: 로그인 기능 구현
    - db 커넥션 전에 email 유효성 검사
    - bcrypt.checkpw로 비밀번호 검사
    - 비밀번호 오류시 401로 처리

--- apps/auth/managers.py
+++ apps/auth/managers.py
@@ -1,7 +1,9 @@
-from django.db.models import Manager
-from django.db.utils  import IntegrityError
+from django.db.models       import Manager
+from django.db.utils        import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
 
 from ..util.exeptions import BadRequestException
+
 
 class UserManager(Manager):
     def create(self, email, password):
@@ -11,3 +13,10 @@ class UserManager(Manager):
 
         except IntegrityError:
             raise BadRequestException('Duplicated email')
+
+    def get_by_email(self, email):
+        try:
+            return self.get(email = email)
+        
+        except ObjectDoesNotExist:
+            raise BadRequestException('Sign up first')
--- apps/auth/views.py
+++ apps/auth/views.py
@@ -3,10 +3,12 @@ import json
 import bcrypt
 
 from django.views import View
-from django.http  import JsonResponse
+from django.http  import JsonResponse, HttpResponse
 
 from .models           import User
 from ..util.validators import validate_email, validate_password
+from ..util.exeptions  import UnauthorizedException
+from ..util.token      import Token
 
 class UserView(View):
     def post(self, request):
@@ -22,3 +24,22 @@ class UserView(View):
         user = User.objects.create(email=email, password=hashed_password)
         
         return JsonResponse({'message' : 'Created'}, status = 201)
+
+class SignInView(View):
+    def post(self, request):
+        body     = json.loads(request.body)
+        email    = body['email']
+        password = body['password']
+
+        validate_email(email)
+
+        user = User.objects.get_by_email(email)
+
+        if not bcrypt.checkpw(password.encode('utf-8'), user.password):
+            raise UnauthorizedException('Invalid password')
+
+        token = Token()
+
+        access_token = token.sign_token(user.id, 'access_token')
+
+        return HttpResponse(headers={'Authorization' : access_token}, status = 204)
