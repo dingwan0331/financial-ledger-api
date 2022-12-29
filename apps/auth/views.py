9e7b077f6e58b70b9116c09a11ba1f79e8e238ca
dingwan <dingwan0331@gmail.com>
2022-12-29 15:49:25 +0900

    refactor: 회원가입 email, password 유효성검사 추가
    - validators.py 에 re 모듈을 활용하여 함수형으로 작성

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -5,13 +5,17 @@ import bcrypt
 from django.views import View
 from django.http  import JsonResponse
 
-from .models import User
+from .models           import User
+from ..util.validators import validate_email, validate_password
 
 class UserView(View):
     def post(self, request):
         body     = json.loads(request.body)
         email    = body['email']
         password = body['password']
+
+        validate_email(email)
+        validate_password(password)
 
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
