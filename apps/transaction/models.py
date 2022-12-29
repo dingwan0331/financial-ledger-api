dcc359195507bf679d0aa1659d9903dece0efc37
dingwan <dingwan0331@gmail.com>
2022-12-29 20:42:17 +0900

    refactor: transaction & Timestamp 모델 리펙토링
    - deposit은 BigInteger로 변경
    - Timestamp 모델을 Float필드로 변경
      - created_at 과 updated_at 모두 unix타임으로 default
      - updated_at은 수정시에 어플리케이션에서 default문 추가해줘야함

--- apps/transaction/models.py
+++ apps/transaction/models.py
@@ -3,9 +3,7 @@ from django.db import models
 from apps.util.models import TimeStampModel
 
 class Transaction(TimeStampModel):
-    deposit          = models.IntegerField()
-    # Index처리를위해 IntegerField 사용
-    transaction_date = models.IntegerField()
+    deposit          = models.BigIntegerField()
     title            = models.CharField(max_length = 20)
     description      = models.CharField(max_length = 100)
     user             = models.ForeignKey('auth.User', on_delete = models.CASCADE)
--- apps/util/models.py
+++ apps/util/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+from datetime import datetime
+
+def timestamp_now():
+    return datetime.now().timestamp()    
+
 class TimeStampModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    created_at = models.FloatField(default = timestamp_now)
+    updated_at = models.FloatField(default = timestamp_now)
     
     class Meta:
         abstract = True
