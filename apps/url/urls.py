29ef4f285f49aaf38d534c432963f3d85650faad
dingwan <dingwan0331@gmail.com>
2022-12-30 19:01:01 +0900

    feat: uuid path로 가게부조회 기능 추가
    -redis를 조회하여 조회가 되는경우 해당 path 로 redirect하도록 구현

--- apps/url/urls.py
+++ apps/url/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.url.views import TransactionSignedUrlView
+from apps.url.views import TransactionSignedUrlView, TransactionRedirectView
 
 urlpatterns = [
     path('/transactions/<int:transaction_id>', TransactionSignedUrlView.as_view()),
+    path('/transactions/<uuid:transaction_uuid>', TransactionRedirectView.as_view()),
 ]
--- apps/url/views.py
+++ apps/url/views.py
@@ -1,22 +1,35 @@
 import uuid
 
-from django.views import View
-from django.http  import JsonResponse
+from django.views     import View
+from django.http      import JsonResponse
+from django.shortcuts import redirect
 
 from django_redis import get_redis_connection
 
-from apps.util.token import verify_token
+from apps.util.token     import verify_token
+from apps.util.exeptions import NotFoundException
+
+redis = get_redis_connection('signed_url')
 
 class TransactionSignedUrlView(View):
     @verify_token
     def get(self, request, transaction_id):
-        redis     = get_redis_connection('signed_url')
         uuid_path = uuid.uuid4()
 
-        signed_url   = f'/transactions/{uuid_path}'
+        signed_url   = f'/urls/transactions/{uuid_path}'
         redirect_url = f'/transactions/{transaction_id}'
         REDIS_EXPIRE = 3600
 
         redis.set(signed_url, redirect_url, REDIS_EXPIRE)
 
         return JsonResponse({'signed_url' : signed_url}, status = 200)
+
+class TransactionRedirectView(View):
+    @verify_token
+    def get(self, request, transaction_uuid):
+        origin_url = redis.get(f'/urls/transactions/{transaction_uuid}')
+        
+        if not origin_url:
+            raise NotFoundException()
+        
+        return redirect(origin_url.decode('utf-8'))
