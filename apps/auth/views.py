de26e0b9efb2bae46306646e651c3e14637f2251
dingwan <dingwan0331@gmail.com>
2022-12-30 20:27:31 +0900

    refactor: 토큰 재발급 api에서 redis에 refresh_token이 없을 시 Invlid token을 반환하도록 수정

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -60,10 +60,7 @@ class TokenView(View):
     def get(self, request):
         access_token  = request.headers.get("Authorization")
         refresh_token = cache.get(access_token)
-
-        if refresh_token == 'logout':
-            raise UnauthorizedException('Invalid token')
-
+        
         token   = Token()
         payload = token.decode_token(refresh_token, 'refresh_token')
         user_id = payload['id']
