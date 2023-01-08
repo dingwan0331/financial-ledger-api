76e2bd15e6387c99a433703038caa8446143f84b
dingwan <dingwan0331@gmail.com>
2023-01-08 12:15:25 +0900

    fix: token.py 중복문 제거

--- apps/util/token.py
+++ apps/util/token.py
@@ -2,32 +2,6 @@ import jwt
 
 from datetime import datetime, timedelta
 
-from config.env import SECRET_KEY, REFRESH_TOKEN_SECRET_KEY
-
-class Token:
-    def __init__(self):
-        self.EXPIRES = {
-            'access_token' : timedelta(hours = 1), 
-            'refresh_token' : timedelta(hours = 8)
-            }
-        self.SECRET_KEYS = {
-            'access_token' : SECRET_KEY , 
-            'refresh_token' : REFRESH_TOKEN_SECRET_KEY
-            }
-        
-    def sign_token(self, user_id, type):
-        expire     = datetime.utcnow() + self.EXPIRES[type]
-        payload    = {"id" : user_id, "exp" : expire }
-        SECRET_KEY = self.SECRET_KEYS[type]
-
-        token = jwt.encode(payload, SECRET_KEY)
-
-        return token
-
-import jwt
-
-from datetime import datetime, timedelta
-
 from django.core.cache import cache
 
 from config.env          import SECRET_KEY, REFRESH_TOKEN_SECRET_KEY
